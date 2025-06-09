c8d0bd8e1537aa54af2b32880028635d07203e42
maxim <maximsmv27@gmail.com>
2025-06-09 21:58:17 +0500

    Add person-service

---
+++ modules/person-service/src/main/java/com/advanced/personservice/PersonServiceApplication.java
@@ -0,0 +1,14 @@
+package com.advanced.personservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PersonServiceApplication.class, args);
+    }
+
+}
